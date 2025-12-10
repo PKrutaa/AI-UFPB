7055e465523c0a9975ef0eff3dddfc9a45d5f9d9
AnaClaraVBandeira <aclaravilarb@gmail.com>
2025-12-10 18:00:01 -0300

    Adicionando o tempo

--- src/word_morphing_bfs.cpp
+++ src/word_morphing_bfs.cpp
@@ -2,15 +2,17 @@
 #include <vector>
 #include <queue>
 #include <algorithm>
+#include <chrono>
 
 using namespace std;
 
 int n = 456976; // 26^4
 
 int cnt_neighborhood = 1;
-vector<int> parent; 
-vector<int> dist; 
+vector<int> parent;
+vector<int> dist;
 
+// convertendo string para número
 int get_index(string s){
     return (s[0]-'A')*26*26*26 +
            (s[1]-'A')*26*26 +
@@ -18,6 +20,7 @@ int get_index(string s){
            (s[3]-'A');
 }
 
+// converte número -> string
 string get_string(int x){
     string s(4,'A');
     s[3] = 'A' + x%26; x/=26;
@@ -30,7 +33,7 @@ string get_string(int x){
 void bfs(string source, string target) {
     for (int i = 0; i <= n; i++) {
         parent[i] = i;
-        dist[i] = -1; 
+        dist[i] = -1;
     }
 
     string v = source;
@@ -44,20 +47,20 @@ void bfs(string source, string target) {
     while (!q.empty()) {
         v = q.front();
         q.pop();
-      
+
         for (int i = 0; i < 4; i++) {
             for (char c = 'A'; c <= 'Z'; c++) {
                 if (c == v[i]) continue;
-                
-                string u = v; 
-                u[i] = c;     
-                
+
+                string u = v;
+                u[i] = c;
+
                 if (dist[get_index(u)] == -1) {
                     cnt_neighborhood++;
                     dist[get_index(u)] = dist[get_index(v)] + 1;
                     q.push(u);
                     parent[get_index(u)] = get_index(v);
-                    if (u == target) return; 
+                    if (u == target) return;
                 }
             }
         }
@@ -71,7 +74,12 @@ int main() {
     string source, target;
     cin >> source >> target;
 
+    auto start = chrono::high_resolution_clock::now();
     bfs(source, target);
+    auto end = chrono::high_resolution_clock::now();
+    // tempo em ms (float)
+    double elapsed_ms = chrono::duration<double, milli>(end - start).count();
+
 
     vector<string> path;
     string curr = target;
@@ -86,6 +94,8 @@ int main() {
 
     cout << "Total nodes processed: " << cnt_neighborhood << endl;
     cout << "Number of transformations: " << dist[get_index(target)] << endl;
+    cout << "Time (ms): " << elapsed_ms << endl;
+
     for (int i = 0; i < path.size(); i++) {
         cout << path[i] << (i == path.size() - 1 ? "\n" : " -> ");
     }
